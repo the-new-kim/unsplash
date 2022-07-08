0b25a518daf2f5ef72ca762a32fb3462cb3bbedd
the-new-kim <jonkim0309@gmail.com>
2022-07-08 21:48:14 +0900

    next js api routes works

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    ACCESS_KEY: process.env.ACCESS_KEY,
+    SECRET_KEY: process.env.SECRET_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
